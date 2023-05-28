0180118329a80040541c4575d6dd0e9ca93916c1
cleverBucket <127633273+cleverBucket@users.noreply.github.com>
2023-05-27 19:41:13 -0700

    winner determined

--- script.js
+++ script.js
@@ -16,10 +16,13 @@ function playerBackgroundColor() {
   if(playerOne) {
     document.getElementById('p1').style.backgroundColor = "#fafab1";
     document.getElementById('p2').style.backgroundColor = 'white';
+    
   }else if(playerOne == false) {
     document.getElementById('p1').style.backgroundColor = "#fff";
-    document.getElementById('p2').style.backgroundColor = "#fafab1";
+    document.getElementById('p2').style.backgroundColor = "#fafab1"; 
+    
   }
+ 
 }
 
 function validateChoice(event) {
@@ -29,17 +32,41 @@ function validateChoice(event) {
   if(squareText !== "X" && squareText !== "O"  && playerOne === true) {
 chosenSquare.innerHTML = "X";
 playerOne = false;
+playerBackgroundColor();
+
   }else{
     if(squareText !== "X" && squareText !== "O"  && playerOne === false) {
       chosenSquare.innerHTML = "O"; 
-      playerOne = true;  
+      playerOne = true; 
+      playerBackgroundColor();
+   
   }
+  
 }
-  return
+ 
 
 }
 
  
+function isThereAWinner() {
+  let elPos1 = document.getElementById('pos1');
+  let elPos2 = document.getElementById('pos2');
+  let elPos3 = document.getElementById('pos3');
+  let elPos4 = document.getElementById('pos4');
+  let elPos5 = document.getElementById('pos5');
+  let elPos6 = document.getElementById('pos6');
+  let elPos7 = document.getElementById('pos7');
+  let elPos8 = document.getElementById('pos8');
+  let elPos9 = document.getElementById('pos9');
+
+  if((elPos1.innerHTML === "X" && elPos2.innerHTML === "X" && elPos3.innerHTML === "X") || (elPos4.innerHTML === "X" && elPos5.innerHTML === "X" && elPos6.innerHTML === "X") || (elPos7.innerHTML === "X" && elPos8.innerHTML === "X" && elPos9.innerHTML === "X") || (elPos1.innerHTML === "X" && elPos4.innerHTML === "X" && elPos7.innerHTML === "X") || (elPos2.innerHTML === "X" && elPos5.innerHTML === "X" && elPos8.innerHTML === "X") || (elPos3.innerHTML === "X" && elPos6.innerHTML === "X" && elPos9.innerHTML === "X") || (elPos1.innerHTML === "X" && elPos5.innerHTML === "X" && elPos9.innerHTML === "X") || (elPos3.innerHTML === "X" && elPos5.innerHTML === "X" && elPos7.innerHTML === "X")) {
+    document.getElementById('p1').innerHTML = "Player One - You are the Winner!";
+
+  }else if((elPos1.innerHTML === "O" && elPos2.innerHTML === "O" && elPos3.innerHTML === "O") || (elPos4.innerHTML === "O" && elPos5.innerHTML === "O" && elPos6.innerHTML === "O") || (elPos7.innerHTML === "O" && elPos8.innerHTML === "O" && elPos9.innerHTML === "O") || (elPos1.innerHTML === "O" && elPos4.innerHTML === "O" && elPos7.innerHTML === "O") || (elPos2.innerHTML === "O" && elPos5.innerHTML === "O" && elPos8.innerHTML === "O") || (elPos3.innerHTML === "O" && elPos6.innerHTML === "O" && elPos9.innerHTML === "O") || (elPos1.innerHTML === "O" && elPos5.innerHTML === "O" && elPos9.innerHTML === "O") || (elPos3.innerHTML === "O" && elPos5.innerHTML === "O" && elPos7.innerHTML === "O")) {
+    document.getElementById('p2').innerHTML = "Player Two - You are the Winner!";
+  }
+}
+  
 
 
 
@@ -50,18 +77,8 @@ function squareClicked(event){
   //put x or o in square
 
   //check to see if they won
-  //change player
-  //change color
-  if(playerOne){
-    document.getElementById(squareID).innerHTML = "X";
-    playerOne = false;
-    playerBackgroundColor();
-  }else if(playerOne == false) {
-    document.getElementById(squareID).innerHTML = "O";
-    playerOne = true;
-    playerBackgroundColor();
-  }
-    return
+  isThereAWinner();
+ 
   
 }
 
